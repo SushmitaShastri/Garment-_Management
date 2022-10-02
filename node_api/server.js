b7f017b0810cc780ad1b2fabe08c4266499a6ea4
SushmitaShastri <54973671+SushmitaShastri@users.noreply.github.com>
2022-10-02 11:43:34 +0530

    Update server.js
    updated vendor delete insert and edit-sushmita

--- node_api/server.js
+++ node_api/server.js
@@ -23,7 +23,7 @@ var dbConn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'garment_management'
+    database: 'garments_management'
 });
   
 // connect to database
@@ -41,8 +41,8 @@ app.post("/login",function(req,res)
     dbConn.query(`select name,username from user where username="${username}" and password="${password}"`
     , function (error, results, fields)
         {
-            console.log("result length: "+results.length);
-            console.log("error "+error);
+           // console.log("result length: "+results.length);
+           // console.log("error "+error);
             if(error)
             {
                 return res.send({error:true,data:results,message:error});
@@ -56,11 +56,67 @@ app.post("/login",function(req,res)
                 console.log("user found");
                 return res.send({error:false,data:[],message:"success"});
             }
-            console.log(results);
+            //console.log(results);
             //console.log(fields);
             
         });
     //console.log(results);
+});
+
+app.get("/vendorList",function(req,res)
+{
+    dbConn.query(`select * from vendor`, function (error, results, fields)
+        {
+            //console.log(results);
+            return res.send({error:false,data:results,message:"success"});
+        });
+});
+
+app.post("/editVendor",function(req,res)
+{
+    //console.log(req);
+    let vid = req.body.vid;
+    let name = req.body.name;
+    let phone = req.body.phone;
+    let address = req.body.address;
+    dbConn.query(`update vendor set name='${name}',phone='${phone}',address='${address}' where vid='${vid}';`, function (error, results, fields)
+        {
+            if(error)
+            {
+                return res.send({error:true,data:results,message:error});
+            }
+            return res.send({error:false,data:results,message:"success"});
+        });
+});
+
+app.post("/deleteVendor",function(req,res)
+{
+    let vid = req.body.vid;
+    //console.log(vid);
+    dbConn.query(`delete from vendor where vid="${vid}"`,function(error,results)
+    {
+        if(error)
+        {
+            return res.send({error:true,data:results,message:error});
+        }
+        return res.send({error:false,data:results,message:"success"});
+    });
+});
+
+app.post("/addVendor",function(req,res)
+{
+    let name = req.body.name;
+    let phone = req.body.phone;
+    let address = req.body.address;
+    dbConn.query(`insert into vendor (admin_id,name,phone,address) values(0,'${name}','${phone}','${address}')`,function(error,results)
+    {
+        if(error)
+        {
+            return res.send({error:true,data:results,message:error});
+        }
+        return res.send({error:false,data:results,message:"success"});
+    });
+
 });
 
 app.listen(port, () => {
