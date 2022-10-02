1173e13a15c3a7322f68a6a23a885d5a36c1e7f5
SushmitaShastri <54973671+SushmitaShastri@users.noreply.github.com>
2022-10-02 12:31:57 +0530

    Update app-routing.module.ts

--- garment-management-application/garment-management/src/app/app-routing.module.ts
+++ garment-management-application/garment-management/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddVendorComponent } from './add-vendor/add-vendor.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditVendorComponent } from './edit-vendor/edit-vendor.component';
 import { LoginComponent } from './login/login.component';
+import { VendorComponent } from './vendor/vendor.component';
 const routes: Routes = 
 [
   {
@@ -16,6 +19,18 @@ const routes: Routes =
     path:'dashboard',
     component:DashboardComponent
   },
+  {
+    path:'vendor',
+    component:VendorComponent
+  },
+  {
+    path:'editvendor',
+    component:EditVendorComponent
+  },
+  {
+    path:'addVendor',
+    component:AddVendorComponent
+  }
 ];
 
 @NgModule({
