a476943218173a497555946c72b1d0ad61a2d24e
SushmitaShastri <54973671+SushmitaShastri@users.noreply.github.com>
2022-10-02 11:42:28 +0530

    Update restapi.service.ts
    vendor api changed sushmita

--- garment-management-application/garment-management/src/app/api_service/restapi.service.ts
+++ garment-management-application/garment-management/src/app/api_service/restapi.service.ts
@@ -7,10 +7,35 @@ import { Observable, of } from 'rxjs';
 })
 export class RestapiService {
   loginUrl = "http://localhost:3000/login";
+  vendorUrl = "http://localhost:3000/vendorList";
+  editVendorUrl = "http://localhost:3000/editVendor";
+  deleteVendorUrl = "http://localhost:3000/deleteVendor";
+  addVendorUrl = "http://localhost:3000/addVendor";
+  vendorData: any;
   constructor(private http: HttpClient) { }
   message:any;
   login(credentials:any):Observable<any[]>
   {
     return this.http.post<any>(this.loginUrl,credentials);
+  }
+  vendorList():Observable<any[]>
+  {
+    return this.http.get<any>(this.vendorUrl);
+  }
+  editVendor(editVendorData:any):Observable<any[]>
+  {
+    return this.http.post<any>(this.editVendorUrl,editVendorData);
+  }
+  setVendorData(vdata:any)
+  {
+    this.vendorData = vdata;
+  }
+  deleteVendor(vid:any)
+  {
+    return this.http.post<any>(this.deleteVendorUrl,vid);
+  }
+  addVendor(addVendorData:any)
+  {
+    return this.http.post<any>(this.addVendorUrl,addVendorData);
   }
 }
